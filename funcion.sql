b6cf94f28feccd4174b9db6446e93f22233126b2
Yopresto <31107220+Yopresto@users.noreply.github.com>
2017-09-06 21:01:31 -0500

    eliana
    prueba

---
+++ funcion.sql
@@ -0,0 +1,27 @@
+﻿-- Function: auditoria.log(character varying, character varying, integer, smallint, character varying, character varying, character varying, integer)
+
+-- DROP FUNCTION auditoria.log(character varying, character varying, integer, smallint, character varying, character varying, character varying, integer);
+
+CREATE OR REPLACE FUNCTION auditoria.log(
+    esquema character varying,
+    tabla character varying,
+    idtabla integer,
+    accion smallint,
+    antiguo character varying,
+    nuevo character varying,
+    ip character varying,
+    usuario integer)
+  RETURNS void AS
+$BODY$
+declare
+begin
+	insert into auditoria.auditorias(esquema, tabla, idtabla, accion, antiguo, nuevo, ip, idusuario)
+	values(esquema, tabla, idtabla, accion, antiguo, nuevo, ip, usuario);
+	return;
+	--esto es una prueba
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION auditoria.log(character varying, character varying, integer, smallint, character varying, character varying, character varying, integer)
+  OWNER TO yp_aarias;
